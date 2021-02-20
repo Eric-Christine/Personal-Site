4d587b5119c5a9c229be63d562b8e1550d5050d0
Eric Chrsitine <ericjchristine@gmail.com>
2021-02-20 14:50:46 -0700

    updated app.js file

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 console.log('connected')
 
+let projectCard = document.querySelector('.card')
+const logo = document.querySelector('#logo')
 
-const projectCard = document.querySelector('card')
-console.log(projectCard)
-const logo = document.querySelector('logo')
-
-
-
-projectCard.addEventListener('click', function spin() {
+projectCard.addEventListener('click', function () {
     console.log('easter egg')
 })
 
-logo.addEventListener('click', function spin() {
-    console.log('easter egg')
-    console.log('Thank you for checking out my website! If you would like to reach out me please email ericjchristine@gmail.com')
+logo.addEventListener('click', function () {
+    console.log('Thank you for checking out my website! If you would like to reach out to me, please email ericjchristine@gmail.com')
 })
 
