90569a7d908988550aa9bff137459b48b1097850
Eric Chrsitine <ericjchristine@gmail.com>
2021-02-19 19:30:56 -0700

    added easter egg

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 console.log('connected')
 
-const projectCard = document.querySelector('.card')
+
+const projectCard = document.querySelector('card')
 console.log(projectCard)
+const logo = document.querySelector('logo')
 
 
 
 projectCard.addEventListener('click', function spin() {
     console.log('easter egg')
 })
+
+logo.addEventListener('click', function spin() {
+    console.log('easter egg')
+    console.log('Thank you for checking out my website! If you would like to reach out me please email ericjchristine@gmail.com')
+})
+
